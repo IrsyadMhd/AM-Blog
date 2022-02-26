f53972a2400264b2ad4155b2a01e95ae6482fbce
irsyadMhd <mhd.irsyad@gmail.com>
2022-02-26 23:32:20 +0700

    edit category page container

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -11,7 +11,7 @@ const CategoryPost = ({ posts }) => {
   }
 
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container px-3 md:px-5 lg:mx-auto lg:px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post, index) => (
